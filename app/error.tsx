a6f53c1d15f0ab0d35c8740a82de2bfe1796a4cc
Mriganka5137 <gogoimriganka09@gmail.com>
2023-11-09 20:57:36 +0530

    Add Error component with reset button

---
+++ app/error.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()} className=" btn mt-5">
+        Try again
+      </button>
+    </div>
+  );
+}
