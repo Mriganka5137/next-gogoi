11972deff7467cdeb8706d479c5ee28353323d78
Mriganka5137 <gogoimriganka09@gmail.com>
2023-11-15 12:05:07 +0530

    Add banner image and configure image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,20 @@ import Link from "next/link";
 import Products from "./components/Products";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import banner from "@/public/sierra-nevada-mountains-macos-high-sierra-mountain-range-5120x2880-8674.jpg";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
-    <main className="text-secondary-content">
-      <h1 className=" text-sky-700 text-3xl">
-        Welcome{" "}
-        {session && (
-          <span className=" text-primary-content text-3xl">
-            {session.user?.name}
-          </span>
-        )}
-      </h1>
-      <ul className=" flex gap-5"></ul>
+    <main className="relative h-screen">
+      <Image
+        src="https://images.pexels.com/photos/691668/pexels-photo-691668.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        fill
+        alt="image"
+        className="object-cover"
+        quality={100}
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+      />
     </main>
   );
 }
